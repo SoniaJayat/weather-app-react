d46f08b0f9462ffc05921943931bf8219f8730d4
SoniaJayat <SoniaJayat03@gmail.com>
2024-07-10 15:19:10 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        <p>
-          This project was coded by{" "}
-          <a
-            href="https://uk.linkedin.com/in/sonia-jayatheesan-9b928b268"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Sonia Jayatheesan
-          </a>{" "}
-          and is {""}
-          <a
-            href="https://github.com/SoniaJayat/weather-app-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://uk.linkedin.com/in/sonia-jayatheesan-9b928b268"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Sonia Jayatheesan
+            </a>{" "}
+            and is {""}
+            <a
+              href="https://github.com/SoniaJayat/weather-app-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
