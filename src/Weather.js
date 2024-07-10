67f7c413dc19842cf54b983ca54059c8bdf75dd6
SoniaJayat <SoniaJayat03@gmail.com>
2024-07-10 21:53:48 +0100

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,35 +4,44 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              className="form-control"
-            />
+      <header>
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="submit btn btn-primary "
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
-          </div>
-        </div>
-      </form>
+        </form>
+      </header>
 
       <h1>Paris</h1>
       <ul>
-        <li>Wednesday 14:30 </li>
-        <li>broken clouds</li>
+        <li>Wednesday 14:30, broken clouds</li>
       </ul>
-      <div className="row">
-        <div className="col-6">
+
+      <div className="row mt-3">
+        <div className="col-6 ">
           <img
             src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
             alt="broken clouds"
           />
-          <h1>26ºC</h1>
+
+          <span className="main-temperature">26</span>
+          <span className="unit">ºC</span>
         </div>
-        <div className="col-6">
+
+        <div className="col-6 ">
           <ul>
             <li>Humidity:46%</li>
             <li>Wind: 5.14km/h</li>
