094a1635079ff7fd0c524b56cfb4f9a5311bf1b0
SoniaJayat <SoniaJayat03@gmail.com>
2024-07-10 14:49:33 +0100

    Added the footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://uk.linkedin.com/in/sonia-jayatheesan-9b928b268"
+            target="_blank"
+          >
+            Sonia Jayatheesan
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/SoniaJayat/weather-app-react"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
 }
