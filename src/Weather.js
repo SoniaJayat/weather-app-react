1130a8d7d2394ad457017c90c67000b7c7b57a5f
SoniaJayat <SoniaJayat03@gmail.com>
2024-07-16 19:02:54 +0100

    Added API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,63 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <header>
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city..."
-                className="form-control"
-              />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      date: "Wednesday 17:45",
+      iconUrl:
+        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <header>
+          <form>
+            <div className="row">
+              <div className="col-9">
+                <input
+                  type="search"
+                  placeholder="Enter a city..."
+                  className="form-control"
+                />
+              </div>
+              <div className="col-3">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="submit btn btn-primary "
+                />
+              </div>
             </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="submit btn btn-primary "
-              />
+          </form>
+        </header>
+
+        <div className="row">
+          <div className="col-6">
+            <h1 className="mt-4">{weatherData.city}</h1>
+            <ul>
+              <div className="weather-app-details"></div>
+              <p className="date-description text-capitalize ">
+                {weatherData.date}, {weatherData.description}
+              </p>
+            </ul>
+          </div>
+
+          <div className="col-6">
+            <div className="main-weather mt-4">
+              <img src={weatherData.iconUrl} alt={weatherData.description} />
+
+              <span className="main-temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">ºC</span>
             </div>
           </div>
-        </form>
-      </header>
-
-      <div className="row">
-        <div className="col-6">
-          <h1 className="mt-4">Paris</h1>
-          <ul>
-            <div className="weather-app-details"></div>
-            <li>Wednesday 14:30, broken clouds</li>
-          </ul>
         </div>
 
-        <div className="col-6">
-          <div className="main-weather mt-4">
-            <img
-              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-              alt="broken clouds"
-            />
-
-            <span className="main-temperature">26</span>
-            <span className="unit">ºC</span>
-          </div>
-        </div>
+        <ul>
+          <li>
+            Humidity:{" "}
+            <strong className="weather-details">{weatherData.humidity}%</strong>
+            , Wind:{" "}
+            <strong className="weather-details">{weatherData.wind}km/h</strong>
+          </li>
+        </ul>
       </div>
+    );
+  } else {
+    const apiKey = "2fe0035acto21f1ea33794b41270a58f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
 
-      <ul>
-        <li>
-          Humidity: <strong className="weather-details">46%</strong>
-        </li>
-        <li>
-          Wind: <strong className="weather-details">5.14km/h</strong>
-        </li>
-        <li>
-          Precipitation: <strong className="weather-details">0%</strong>
-        </li>
-      </ul>
-    </div>
-  );
+    return "Loading...";
+  }
 }
