63420bb552da882a88a6ce3fb8d374b7930c511d
SoniaJayat <SoniaJayat03@gmail.com>
2024-08-07 21:47:13 +0100

    Created 1 day forecast with api

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,63 +1,36 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
     console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
-  console.log(props);
-  let apiKey = `2fe0035acto21f1ea33794b41270a58f`;
-  let longitude = props.coordinates.longitude;
-  let latitude = props.coordinates.latitude;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&unit=metric
-`;
 
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast mt-5">
-      <div className="row">
-        <div className="col">
-          <div className="WeatherForecast-day">Sat</div>
-          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"></img>
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max"> 19º</span>
-            <span className="WeatherForecast-temperature-min"> 15º</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Sat</div>
-          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"></img>
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max"> 19º</span>
-            <span className="WeatherForecast-temperature-min"> 15º</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Sat</div>
-          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"></img>
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max"> 19º</span>
-            <span className="WeatherForecast-temperature-min"> 15º</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Sat</div>
-          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"></img>
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max"> 19º</span>
-            <span className="WeatherForecast-temperature-min"> 15º</span>
-          </div>
-        </div>
-        <div className="col">
-          <div className="WeatherForecast-day">Sat</div>
-          <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"></img>
-          <div className="WeatherForecast-temperatures">
-            <span className="WeatherForecast-temperature-max"> 19º</span>
-            <span className="WeatherForecast-temperature-min"> 15º</span>
-          </div>
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast mt-5">
+        <div className="row">
+          <WeatherForecastDay data={forecast[0]} />
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = `2fe0035acto21f1ea33794b41270a58f`;
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&unit=metric
+`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
 }
